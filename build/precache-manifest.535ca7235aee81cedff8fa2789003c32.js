18f44149e08d7633b17c3ff710c0bb88df83a9c4
Koval Vladislav <koval.vlad@softservice.org>
2020-09-20 15:00:14 +0700

    build 1.0

---
+++ build/precache-manifest.535ca7235aee81cedff8fa2789003c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "596b61d1743bec87879b82fa0d933f52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "86049e50eaa93942049f",
+    "url": "/static/css/main.5a9b757d.chunk.css"
+  },
+  {
+    "revision": "79951507f8b7db7975d4",
+    "url": "/static/js/2.d28820b3.chunk.js"
+  },
+  {
+    "revision": "62c1645b53df7f873fca458c71c36e8c",
+    "url": "/static/js/2.d28820b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86049e50eaa93942049f",
+    "url": "/static/js/main.ee6e8954.chunk.js"
+  },
+  {
+    "revision": "7489aa6599b46d104352",
+    "url": "/static/js/runtime-main.12fbb3b5.js"
+  }
+]);
